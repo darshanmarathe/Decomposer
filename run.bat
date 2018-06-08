b997988167094fa0a9f77fc9c43eb58d112a8c96
D24809 <darshanmarathe@gmail.com>
2018-06-08 22:14:16 +0530

    tweet api

--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
 cls
-node index.js sampleData\tweets.bson map.js 
-node index.js sampleData\tweets.bson map.array.js 
+
+node index.js sampleData\visitors.bson tweetmap.js 1 
+
+rem node index.js sampleData\tweets.bson map.js 
+rem node index.js sampleData\tweets.bson map.array.js 
--- tweetmap.js
+++ tweetmap.js
@@ -1,6 +1,19 @@
 function map(document) {
-    console.log(JSON.stringify(document , 4 , null))
-    
+    console.log(JSON.stringify(document, 4, null))
+    for (const key in document) {
+        if (document.hasOwnProperty(key)) {
+            const element = document[key];
+            if (typeof element === "object") {
+                if (Array.isArray(element)) {
+                    console.log("Array" , key)
+                }else{
+                    console.log("object" , key)
+                }
+            }
+        }
+    }
+
+
     return document;
 }
 exports.map = map;
