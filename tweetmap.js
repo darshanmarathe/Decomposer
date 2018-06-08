6b7542b02cc06c122737757245eceea44c05525c
D24809 <darshanmarathe@gmail.com>
2018-06-08 22:21:04 +0530

    tweet api

--- tweetmap.js
+++ tweetmap.js
@@ -1,5 +1,5 @@
 function map(document) {
-    console.log(JSON.stringify(document, 4, null))
+    console.log(document)
     for (const key in document) {
         if (document.hasOwnProperty(key)) {
             const element = document[key];
@@ -7,7 +7,7 @@ function map(document) {
                 if (Array.isArray(element)) {
                     console.log("Array" , key)
                 }else{
-                    console.log("object " , key)
+                    console.log("object" , key)
                 }
             }
         }
